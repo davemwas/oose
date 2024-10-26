555f02481ad4e56288796813d58c921e2d2fd023
David Mwangi Thiong'o <david.thiongo@vodafone.com>
2024-10-26 15:45:59 +0300

    Update values

--- class2/src/MainApplication.java
+++ class2/src/MainApplication.java
@@ -1,7 +1,7 @@
 public class MainApplication {
     public static void main(String[] args){
-        Admin curAdmin = new Admin(1234, "Stephan", "xa1@LW42%3");// instantiation
-        User curUser = new User(4455, "John");
+        Admin curAdmin = new Admin(1234, "David", "KzWTUVJ7rt");// instantiation
+        User curUser = new User(1224, "Luke");
         curAdmin.performOperation(new Update()); // Admin Updates the data.
         curUser.performOperation(new View()); // User can view the data.
     }
